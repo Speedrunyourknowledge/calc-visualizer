78024b077f19c9a5b53f3bbb46a0e9a4abf462c9
Fernando <crespofernando05@gmail.com>
2025-02-14 16:41:43 -0500

    update error page styling

--- calc-frontend/src/pages/ErrorPage.tsx
+++ calc-frontend/src/pages/ErrorPage.tsx
@@ -4,10 +4,10 @@ function ErrorPage(){
     const navigate = useNavigate();
 
     return (
-        <div>
-            <h1 className="text-5xl">Oops!</h1>
-            <p>This page doesn't seem to exist</p>
-            <p>You may have mistyped the address or the page may have moved</p>
+        <div className="flex flex-col justify-start items-center gap-10">
+            <h1 className="error-title">Oops!</h1>
+            <p className="font-medium text-3xl">Looks like the limit of this page doesn't exist.</p>
+            <p className="font-medium text-3xl">You may have mistyped the address or the page may have moved.</p>
             <button className="back-button" onClick={()=> navigate("/")}>Back to Home</button>
         </div>
     )
