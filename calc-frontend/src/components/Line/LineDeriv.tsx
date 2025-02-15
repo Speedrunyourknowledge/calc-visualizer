c9f7310f3f60ab62e48d96470c0de1da76d66f16
Anthony Vargas <90121982+Speedrunyourknowledge@users.noreply.github.com>
2025-02-14 21:09:11 -0500

    Update logo and page styling

--- calc-frontend/src/components/Line/LineDeriv.tsx
+++ calc-frontend/src/components/Line/LineDeriv.tsx
@@ -13,7 +13,7 @@ function LineDeriv()
           <h2 style={{marginBottom:'1rem'}}>Derivative of &nbsp;{"\\(y = \\frac{1}{2} x\\)"} </h2>
         </MathJax>
         
-        <iframe src="https://www.desmos.com/calculator/eugh1m4swm?embed" width="500" height="500" 
+        <iframe src="https://www.desmos.com/calculator/7fswj3rgdh?embed" width="500" height="500" 
                 style={{border: "1px solid #ccc"}} >
         </iframe>
         
