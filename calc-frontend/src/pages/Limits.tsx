96a7fc988470c911a8347f33cf01b4a71c16edad
Fernando <crespofernando05@gmail.com>
2025-02-16 21:11:06 -0500

    fix formatting

--- calc-frontend/src/pages/Limits.tsx
+++ calc-frontend/src/pages/Limits.tsx
@@ -6,22 +6,22 @@ function Limits() {
 
   const navigate = useNavigate();
 
-    return (
-        <div>
-            <button className="back-button" onClick={()=> navigate(-1)}>&#8249; Back</button>
+  return (
+    <div>
+      <button className="back-button" onClick={() => navigate(-1)}>&#8249; Back</button>
 
-            <div className="flex flex-col flex-grow justify-center gap-6 items-center">
-            <h2 style={{marginBottom:'1rem' }}>The Limit of Common Functions</h2>
+      <div className="flex flex-col flex-grow justify-center gap-6 items-center">
+        <h2 style={{ marginBottom: '1rem' }}>The Limit of Common Functions</h2>
 
-              <Link to="/limits/parabola" className="topic-box">
-              <button className="px-10 text-2xl cursor-pointer">Parabola</button>
-              </Link>
+        <Link to="/limits/parabola" className="topic-box">
+          <button className="px-10 text-2xl cursor-pointer">Parabola</button>
+        </Link>
 
 
-            </div>
-        </div>
+      </div>
+    </div>
 
-    )
+  )
 }
 
 export default Limits
