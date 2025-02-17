9154b7c06cb10e83c474b1d6c3f6542d052e2174
Fernando <crespofernando05@gmail.com>
2025-02-16 20:19:27 -0500

    update to Link tag derivatives page

--- calc-frontend/src/pages/Derivatives.tsx
+++ calc-frontend/src/pages/Derivatives.tsx
@@ -1,34 +1,54 @@
 import { useNavigate } from "react-router"
+import { Link } from "react-router"
+
 
 // will show list of derivative options to choose from
-function Derivatives(){
+function Derivatives() {
 
   const navigate = useNavigate();
 
-    return (
-        <div>
-          <button className="back-button" onClick={()=> navigate(-1)}>&#8249; Back</button>
-          <h2 style={{marginBottom:'1rem' }}>The Derivative of Common Functions</h2>
+  return (
+    <div>
+      <button className="back-button" onClick={() => navigate(-1)}>&#8249; Back</button>
+      <h2 style={{ marginBottom: '1rem' }}>The Derivative of Common Functions</h2>
 
-          <div className="flex flex-col flex-grow justify-center gap-6">
-            <button className="topic-button" onClick={()=> navigate("/derivatives/linear")}>Linear</button>
-            
-            <button className="topic-button" onClick={()=> navigate("/derivatives/parabola")}>Parabola</button>
+      <div className="flex flex-col flex-grow justify-center gap-6">
 
-            <button className="topic-button" onClick={()=> navigate("/derivatives/cubic")}>Cubic</button>
+        <Link to="/derivatives/linear">
+          <button className="topic-button">Linear</button>
+        </Link>
 
-            <button className="topic-button" onClick={()=> navigate("/derivatives/euler")}>Euler</button>
+        <Link to="/derivatives/parabola">
+          <button className="topic-button">Parabola</button>
+        </Link>
 
-            <button className="topic-button" onClick={()=> navigate("/derivatives/natural-log")}>Natural Log</button>
+        <Link to="/derivatives/cubic">
+          <button className="topic-button">Cubic</button>
+        </Link>
 
-            <button className="topic-button" onClick={()=> navigate("/derivatives/sine")}>Sine</button>
+        <Link to="/derivatives/euler">
+          <button className="topic-button">Euler</button>
+        </Link>
 
-            <button className="topic-button" onClick={()=> navigate("/derivatives/cosine")}>Cosine</button>
+        <Link to="/derivatives/natural-log">
+          <button className="topic-button">Natural Log</button>
+        </Link>
 
-            <button className="topic-button" onClick={()=> navigate("/derivatives/tangent")}>Tangent</button>
-          </div>
-        </div>
-    )
+        <Link to="/derivatives/sine">
+          <button className="topic-button">Sine</button>
+        </Link>
+
+        <Link to="/derivatives/cosine">
+          <button className="topic-button">Cosine</button>
+        </Link>
+
+        <Link to="/derivatives/tangent">
+          <button className="topic-button">Tangent</button>
+        </Link>
+
+      </div>
+    </div>
+  )
 }
 
 export default Derivatives
