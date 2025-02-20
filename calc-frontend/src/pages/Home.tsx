c50ed32ea15acba20ba7b8927154f1404537aa18
Anthony Vargas <90121982+Speedrunyourknowledge@users.noreply.github.com>
2025-02-19 20:01:31 -0500

    Update page color and buttons

--- calc-frontend/src/pages/Home.tsx
+++ calc-frontend/src/pages/Home.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router"
 function Home() {
 
   return (
+    <div>
+      <h2 className = "topic-header m-auto w-fit">Topics</h2>
+
       <div className="flex flex-col gap-6 m-auto home-list">
           <Link to="/derivatives" className = "link-box">
               <div className="link-title">Derivatives</div>
@@ -17,6 +20,7 @@ function Home() {
               <div className="link-title">Unit Circle</div>
           </Link>
       </div>
+    </div>
   )
 };
 
