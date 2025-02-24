dc91a4a0aaf15808ec5fd43d8af6fb1f81c2d8be
GeorgeAvdella <166246086+GeorgeAvdella@users.noreply.github.com>
2025-02-24 17:29:17 -0500

    Update Home.tsx
    Just changed the order to limits, derivatives, then integrals since this is the order it taught

--- calc-frontend/src/pages/Home.tsx
+++ calc-frontend/src/pages/Home.tsx
@@ -7,6 +7,11 @@ function Home() {
       <h2 className = "topic-header m-auto w-fit">Topics</h2>
 
       <div className="flex flex-col gap-6 m-auto home-list">
+
+          <Link to="/limits" className = "link-box">
+              <div className="link-title">Limits</div>
+          </Link>
+        
           <Link to="/derivatives" className = "link-box">
               <div className="link-title">Derivatives</div>
           </Link>
@@ -14,10 +19,7 @@ function Home() {
           <Link to="/integrals" className = "link-box">
               <div className="link-title">Integrals</div>
           </Link>
-          <Link to="/limits" className = "link-box">
-              <div className="link-title">Limits</div>
-          </Link>
-          
+        
           <Link to="/unit-circle" className = "link-box">
               <div className="link-title">Unit Circle</div>
           </Link>
