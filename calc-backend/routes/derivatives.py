7005a5a9cbd891145132177a14fc3aa325d88838
Fernando <crespofernando05@gmail.com>
2025-02-27 14:29:41 -0500

    mock graph

--- calc-backend/routes/derivatives.py
+++ calc-backend/routes/derivatives.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+import numpy as np
+import plotly.graph_objects as go
 #from services.derivative_calc import {functionName} # uncomment to use functions from services
 
 derivatives_bp = Blueprint('derivatives', __name__)
@@ -9,4 +11,16 @@ derivatives_bp = Blueprint('derivatives', __name__)
 # http://127.0.0.1:5000/api/custom-derivative
 @derivatives_bp.route('/custom-derivative', methods=["GET"])
 def derivative_graph():
-    return "custom derivative graph"
+    x = np.linspace(-10, 10, 400)
+    y = x ** 2
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='y = x^2'))
+
+    fig.update_layout(
+        title = "Quadratic Function: y = x^2",
+        xaxis_title="x",
+        yaxis_title="y",
+        template="plotly_white",
+    )
+    return jsonify(fig.to_json())
