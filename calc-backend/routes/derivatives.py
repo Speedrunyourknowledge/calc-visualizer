4d0734089602ab29d93dc137deaba8a7bbaf9500
Fernando <crespofernando05@gmail.com>
2025-02-27 17:03:41 -0500

    set up logic for dynamic tangent line

--- calc-backend/routes/derivatives.py
+++ calc-backend/routes/derivatives.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 import numpy as np
 import plotly.graph_objects as go
 #from services.derivative_calc import {functionName} # uncomment to use functions from services
@@ -14,13 +14,25 @@ def derivative_graph():
     x = np.linspace(-10, 10, 400)
     y = x ** 2
 
+    x0 = float(request.args.get("x0", 0))
+
+    y_prime = 2 * x0
+    y_tangent = y_prime * (x - x0) + (x0 **2)
+
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='y = x^2'))
+
+    fig.add_trace(go.Scatter(x=x, y=y_tangent, mode='lines', name=f"Tangent at x = {x0:.2f}", line=dict(dash='dash')))
+
+    fig.add_trace(go.Scatter(x=[x0], y=[x0**2], mode='markers', name=f"Point at x = {x0:.2f}", marker=dict(color='red', size=10)))
+
 
     fig.update_layout(
         title = "Quadratic Function: y = x^2",
         xaxis_title="x",
         yaxis_title="y",
-        template="plotly_white",
+        template="plotly_dark",
+        xaxis = dict(range=[-10, 10]),
+        yaxis = dict(range=[0,100]),
     )
-    return jsonify(fig.to_json())
+    return jsonify(fig.to_dict())
