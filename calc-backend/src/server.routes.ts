c7915663570ecda44bb42f9223fcde944d155a43
Fernando <crespofernando05@gmail.com>
2025-03-08 14:22:17 -0500

    Update api routes

--- calc-backend/src/server.routes.ts
+++ calc-backend/src/server.routes.ts
@@ -7,12 +7,13 @@ import userRoutes from "./routes/user.routes";
 
 const router = Router();
 
+// first prefix each route with /api and then:
 // prefix each route with what it will do: auth, user
 
 // Prefixes the endpoint you created with what you specify here
-//router.use("/auth", authRoutes);
+//router.use("/api/auth", authRoutes);
 
-// will be localhost:3000/user/{whatever you chose in routes directory}
-router.use("/user", userRoutes); 
+// will be localhost:3000/api/user/{whatever you chose in routes directory}
+router.use("/api/user", userRoutes);
 
 export default router;
