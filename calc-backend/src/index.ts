646b7c6b70f2dd28228000fee3aa70111b616d84
Fernando <crespofernando05@gmail.com>
2025-03-19 00:11:03 -0400

    Set up better-auth and db

--- calc-backend/src/index.ts
+++ calc-backend/src/index.ts
@@ -3,10 +3,14 @@ import express from "express";
 import cors from "cors";
 import routes from "./server.routes";
 import errorMiddleware from "./middlewares/errorHandler.middleware";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.all("/api/auth/*", toNodeHandler(auth));
 
 app.use(cors());
 app.use(express.json())
