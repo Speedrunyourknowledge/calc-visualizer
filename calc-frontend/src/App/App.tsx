6477ac39d562ef6bed582f12db65c05f6c8cbb3a
Fernando <crespofernando05@gmail.com>
2025-03-25 18:04:01 -0400

    Create useContext to fetch auth info on client

--- calc-frontend/src/App/App.tsx
+++ calc-frontend/src/App/App.tsx
@@ -1,26 +1,26 @@
-import { createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router";
 
 import '../../node_modules/mathquill/build/mathquill.css'
 import './app.css'
 import './widgets.css'
 
 import RoutesList from "./RoutesList";
+import { AuthProvider } from "./AuthContext";
 
 
 function App() {
 
-    // go to the RootLayout component to edit the visual layout
+  // go to the RootLayout component to edit the visual layout
 
-    // Handles all the routes
-    // The calc-visualizer path is for github pages
-    const router = createBrowserRouter(
-      createRoutesFromElements(
-        RoutesList
-      )
-    );
+  // Handles all the routes
+  // The calc-visualizer path is for github pages
+  const router = createBrowserRouter(
+    createRoutesFromElements(RoutesList));
 
   return (
+    <AuthProvider>
       <RouterProvider router={router} />
+    </AuthProvider>
   )
 }
 
