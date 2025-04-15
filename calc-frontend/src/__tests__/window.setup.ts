968a4426f1069524941d5bf5790713324d999e49
Anthony Vargas <90121982+Speedrunyourknowledge@users.noreply.github.com>
2025-04-15 08:53:56 -0400

    fix: window error with vitest

--- calc-frontend/src/__tests__/window.setup.ts
+++ calc-frontend/src/__tests__/window.setup.ts
@@ -2,3 +2,17 @@ import { vi } from 'vitest'
 
 // prevents jsdom error
 window.scrollTo = vi.fn
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query: unknown) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
