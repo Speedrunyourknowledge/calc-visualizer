6a54d9adf195e4cb6b15d2a100f50ed2209f22b3
George Avdella <gavdella30@gmail.com>
2025-04-15 23:36:39 -0400

    setup deriv files

---
+++ calc-frontend/src/components/Sine/SineDerivGraph.tsx
@@ -0,0 +1,15 @@
+import { useLayoutEffect } from "react";
+
+function SineDerivGraph() {
+   useLayoutEffect(() =>{
+
+  }, []);
+
+  return (
+  <div className="plotly-graph-div graph-frame" id="">
+  </div>
+  )
+
+}
+
+export default SineDerivGraph
