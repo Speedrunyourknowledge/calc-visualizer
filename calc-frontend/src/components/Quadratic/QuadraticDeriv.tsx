782f6b723d8474537d5e6d43ebdb282acca5d1fc
George Avdella <gavdella30@gmail.com>
2025-04-15 23:28:59 -0400

    setup deriv files

--- calc-frontend/src/components/Quadratic/QuadraticDeriv.tsx
+++ calc-frontend/src/components/Quadratic/QuadraticDeriv.tsx
@@ -1,34 +1,24 @@
 import { useLayoutEffect, useRef } from "react";
+import QuadraticDerivGraph from "./QuadraticDerivGraph"
 
-function QuadraticDeriv(){
+function QuadraticDeriv() {
 
-  const heading = useRef(null);
+  const container = useRef(null);
 
   useLayoutEffect(() =>{
     //@ts-ignore
     let MQ = MathQuill.getInterface(2);
-
-    MQ.StaticMath(heading.current, { })
+    MQ.StaticMath(container.current, { })
 
   }, []);
 
   return (
-      <div>
-        <div>
-          <h2 className="center-header" style={{marginBottom:'0.5rem'}}>Derivative of <span ref={heading}>y = x^&#123;2&#125;</span> </h2>
-        </div>
+ 
 
-        <div className="graph-outer-box">
-          <iframe className = "graph-frame" src="https://www.desmos.com/calculator/wtqxy7h0yi?embed"
-                  style={{border: "1px solid #ccc"}} >
-          </iframe>
-
-          <p className="big-p side-text">
-            The quadratic function squares each input value, raising it to the second power
-          </p>
-        </div>
-        
+      <div className="graph-outer-box" >
+        <QuadraticDerivGraph />
       </div>
+
   )
 }
 
--- calc-frontend/src/components/Sine/SineDeriv.tsx
+++ calc-frontend/src/components/Sine/SineDeriv.tsx
@@ -1,36 +1,24 @@
 import { useLayoutEffect, useRef } from "react";
+import SineDerivGraph from "./SineDerivGraph"
 
-function SineDeriv(){
+function SineDeriv() {
 
-  const heading = useRef(null);
   const container = useRef(null);
 
   useLayoutEffect(() =>{
     //@ts-ignore
     let MQ = MathQuill.getInterface(2);
-    MQ.StaticMath(heading.current, { })
     MQ.StaticMath(container.current, { })
 
   }, []);
 
   return (
-    <div> 
-      <div>
-        <h2 className="center-header" style={{marginBottom:'0.5rem'}}>Derivative of <span ref={heading}>y = sin(x)</span> </h2>
-      </div>
-      
-      <div className="graph-outer-box">
-        <iframe className = "graph-frame" src="https://www.desmos.com/calculator/e9waw1mka6?embed"  
-                style={{border: "1px solid #ccc"}} >
-        </iframe>
+ 
 
-        <p className="big-p side-text">
-          The sine function starts with a value of 0 and ends with a value of 0 on the 
-          interval <span ref={container}>[0, \quad 2\pi]</span>
-        </p>
+      <div className="graph-outer-box" >
+        <SineDerivGraph />
       </div>
-    
-    </div>
+
   )
 }
 
